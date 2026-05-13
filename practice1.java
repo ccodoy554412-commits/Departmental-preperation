366826912203c4e27f56dd74fe47d1908816d83b
C.Charles <c.codoy.554412@umindanao.edu.ph>
2026-05-13 13:25:25 +0800

    test1

---
+++ practice1.java
@@ -0,0 +1,11 @@
+package Departmental_test;
+
+import javax.swing.*;
+public class practice1 extends JFrame {
+    practice1(){
+
+    }
+    public static void main(String[] args) {
+        new practice1();       
+    }
+}
