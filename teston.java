0df2a0d82628ee3689f2d79550d5c15d882bdbdc
C.Charles <c.codoy.554412@umindanao.edu.ph>
2026-05-14 17:25:53 +0800

    serve as practice

---
+++ teston.java
@@ -0,0 +1,10 @@
+package Departmental_test;
+
+public class teston {
+    public static void main(String[] args) {
+       String[] name = {"bob","alice"}; 
+       for(String names : name){
+        System.out.println(names);
+       }
+    }
+}
